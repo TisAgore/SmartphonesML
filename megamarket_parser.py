d7444f552b14b7a3fdeed6eac30c41cd13b75726
TisAgore <97605431+TisAgore@users.noreply.github.com>
2024-06-06 18:15:00 +0500

    Декоративные изменения

--- megamarket_parser.py
+++ megamarket_parser.py
@@ -1,6 +1,5 @@
 import json
 from urllib import parse
-import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,13 +9,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ChromeOptions
 
-
 BASEURL = 'https://megamarket.ru'
 
-
-def get_pages_html(url, model_name):
-    # options = Options()
-    # options.add_argument('--ignore-certificate-errors')
+def get_pages_html(url: str, model_name: str):
     options = ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -25,7 +20,7 @@ def get_pages_html(url, model_name):
     ITEMS_PRICES = []
     try:
         driver.get(url=url)
-        WebDriverWait(driver, 60).until(
+        WebDriverWait(driver, 10).until(
             ec.presence_of_element_located((By.TAG_NAME, "html")))
         get_items(driver.page_source, ITEMS, model_name, ITEMS_PRICES)
     except Exception as ex:
@@ -58,52 +53,21 @@ def get_items(html: str, items: dict, model_name: str, prices: list[int]):
             continue
         items[int(item_price)] = link
         prices.append(int(item_price))
-        # items.append({
-        #     'Наименование': item_title,
-        #     'Цена': int(item_price),
-        # # 'Сумма бонуса': bonus,
-        # # 'Процент бонуса': bonus_percent,
-        #     'Ссылка на товар': link
-        # })
-    return True
 
 
-# def save_excel(data: list):
-#     """сохранение результата в excel файл"""
-#     # df = pd.DataFrame(data)
-#     # print(df['Цена'])
-#     print(min(data))
-    # writer = pd.ExcelWriter(f'{filename}.xlsx')
-    # df.to_excel(writer, sheet_name='data', index=False)
-    # указываем размеры каждого столбца в итоговом файле
-    # writer.sheets['data'].set_column(0, 1, width=50)
-    # writer.sheets['data'].set_column(1, 2, width=30)
-    # writer.sheets['data'].set_column(2, 3, width=8)
-    # writer.sheets['data'].set_column(3, 4, width=20)
-    # writer.sheets['data'].set_column(4, 5, width=15)
-    # writer.close()
-    # print(f'Все сохранено в {filename}.xlsx')
-
-
-def main(target: str):
-    # target = input('Введите название товара: ')
-    # min_price = input('Минимальная цена (enter, чтобы пропустить): ')
-    # min_price = min_price if min_price != '' else '0'
-    # max_price = input('Максимальная цена (enter, чтобы пропустить): ')
-    # max_price = max_price if max_price != '' else '9999999'
-    target_url = f"{BASEURL}/catalog/?q={target}&collectionId=12546"
+def main(model_name: str):
+    model_name_url = f"{BASEURL}/catalog/?q={model_name}&collectionId=12546"
     filter = {
-        # "88C83F68482F447C9F4E401955196697": {"min": int(min_price), "max": int(max_price)},# фильтр по цене
-        "4CB2C27EAAFC4EB39378C4B7487E6C9E": ["1"]}# фильтр по наличию товара
+        "4CB2C27EAAFC4EB39378C4B7487E6C9E": ["1"]} # фильтр по наличию товара
     json_data = json.dumps(filter)
     # Кодирование JSON строки для передачи через URL
     url_encoded_data = parse.quote(json_data)
-    target_url += '#?filters=' + url_encoded_data
+    model_name_url += '#?filters=' + url_encoded_data
 
-    items, prices = get_pages_html(target_url, target)
-    min_item_link = items[min(prices)]
-    return min_item_link, min(prices)
-
+    items, prices = get_pages_html(model_name_url, model_name)
+    min_price = min(prices)
+    min_item_link = items[min_price]
+    return min_item_link, min_price
 
 if __name__ == '__main__':
     lnk, price = main('iphone 15 128')
